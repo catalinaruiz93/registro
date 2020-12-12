a6d06fd8f1b88ec6480b1474a37ed1c755b077f5
catalinaruiz93 <icruiz73@misena.edu.co>
2020-12-11 20:44:47 -0500

    subi archivos

---
+++ login.js
@@ -0,0 +1,25 @@
+function validar_correo(){}
+var correo = document.getElementById("correo").value;
+var contraseña = document.getElementById("contraseña").value;
+var usuario={
+    usuario: "icruiz73@misena.edu.co",
+    contraseña:"1234",
+}
+
+var correo="icruiz73@misena.edu.co";
+if (correo =='icruiz73@misena.edu.co'){
+    
+    
+}
+
+ 
+
+else {
+    alert("El correo no es correcto");
+
+}
+
+
+
+
+
